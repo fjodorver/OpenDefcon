12ba2a306187c6393047823f2372be52a2db1447
Fjodor Vershinin <fjodor@vershinin.net>
2016-09-18 15:04:41 +0300

    Initial commit

---
+++ desktop/src/net/vershinin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.vershinin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.vershinin.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
