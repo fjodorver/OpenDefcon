5bcbb66f97cccbfab9430afc9599417c84c92003
Fjodor Vershinin <fjodor@vershinin.net>
2016-09-21 14:15:53 +0300

    Added feature: zoom

--- core/src/net/vershinin/DefconViewportController.java
+++ core/src/net/vershinin/DefconViewportController.java
@@ -1,16 +1,21 @@
 package net.vershinin;
 
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class DefconViewportController extends InputAdapter {
 
     private InputListener inputListener;
+    private Viewport v1, v2;
 
     public DefconViewportController(final Viewport viewport1, final Viewport viewport2) {
+        v1 = viewport1;
+        v2 = viewport2;
         inputListener = new DragListener(){
             @Override
             public void drag(InputEvent event, float x, float y, int pointer) {
@@ -39,5 +44,27 @@ public class DefconViewportController extends InputAdapter {
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         inputListener.touchDragged(new InputEvent(), screenX, screenY, pointer);
         return true;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        update((ScalingViewport) v1, 2 * amount);
+        update((ScalingViewport) v2, 2 * amount);
+        v2.setScreenX(v1.getScreenX() + v1.getScreenWidth());
+        if(v1.getScreenX() > 0){
+            v2.setScreenX(v1.getScreenX() - v1.getScreenWidth());
+        }
+        return true;
+    }
+
+    private void update(ScalingViewport scalingViewport, int amount){
+        int screenW = scalingViewport.getScreenWidth();
+        int screenH = scalingViewport.getScreenHeight();
+        float worldW = scalingViewport.getWorldWidth();
+        float worldH = scalingViewport.getWorldHeight();
+        Vector2 scaled = scalingViewport.getScaling().apply(worldH, worldW, screenW-amount, screenH-amount);
+        int viewportWidth = Math.round(scaled.x);
+        int viewportHeight = Math.round(scaled.y);
+        scalingViewport.setScreenSize(viewportWidth, viewportHeight);
     }
 }
